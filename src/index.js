26a21e8588d7714314e0bed401d55a0422746291
Marianchuk <marianchuksv@gmail.com>
2022-02-17 16:43:59 +0200

    add some js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let open = [];
+  let pair = {};
+  let stack = [];
+  
+
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    pair[bracketsConfig[j][1]] = bracketsConfig[j][0];
+    open.push(bracketsConfig[j][0]);
+  }
+
+    for (let i = 0; i < str.length; i++) {
+
+          if(open.includes(str[i])) {
+            stack.push(str[i]);
+          } else {
+                if (stack.length === 0) {
+                  return false;
+                }
+                              
+                if(pair[str[i]] === (stack[stack.length - 1])) {
+                  stack.pop();
+                } else {
+                  return false;
+                } 
+            }
+        } 
+        return stack.length === 0
+  }
+  
+
+
